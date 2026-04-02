5e1b102c590ba31cf05e48ec9b894d4cd1e98cfe
unibrind <mebarekouldmammar95@gmail.com>
2026-04-02 04:44:01 +0200

    4-feat: implement LeafNode and add comprehensive unit tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase): # On hérite de TestCase
     def test_props_to_html(self):
@@ -20,6 +20,23 @@ class TestHTMLNode(unittest.TestCase): # On hérite de TestCase
         # Test que le __repr__ renvoie bien une chaîne de caractères
         node = HTMLNode("h1", "Titre")
         self.assertTrue(node.__repr__().startswith("HTMLNode(h1, Titre"))
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://google.com">Click me!</a>')
+    
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
 
 if __name__ == "__main__":
     unittest.main()
